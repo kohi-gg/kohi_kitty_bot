7c10215953b143a1db3f802edc875f05cd73736c
serjeph <engr.jeffmanzanillo@gmail.com>
2024-11-05 20:18:39 +0800

    added discord presence

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 require('dotenv').config({ path: './.env' });
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -29,6 +29,15 @@ for (const folder of commandFolders) {
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+	
+	// Set the presence
+	c.user.setPresence({ 
+		activities: [{ 
+			name: '/kohi-help', 
+			type: ActivityType.Playing 
+		}], 
+		status: 'online' 
+	});
 });
 
 client.on(Events.InteractionCreate, async interaction => {

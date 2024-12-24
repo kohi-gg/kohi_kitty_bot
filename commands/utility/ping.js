2ef89fea40ab3bc2b6726efb01e5a82bf4b0427b
chesidibols <59624109+chesidibols@users.noreply.github.com>
2024-12-24 18:40:58 +0800

    Create ping.js

---
+++ commands/utility/ping.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+      .setName('ping')
+      .setDescription('test ping'),
+    async execute(interaction) {
+        await interaction.deferReply();
+        await interaction.deleteReply();
+
+        const m = await interaction.channel.send("Ping?");
+    m.edit(`Pong isama ang <:tuna:1320596587373658195> ${m.createdTimestamp - interaction.createdTimestamp}ms`);
+    
+   
+    },
+};

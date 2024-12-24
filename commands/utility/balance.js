f16edf9ea9a7a2ad80a8345270aee90f8f81c274
chesidibols <59624109+chesidibols@users.noreply.github.com>
2024-12-25 07:52:29 +0800

    Update balance.js
    check balance of user, not yet properly working

--- commands/utility/balance.js
+++ commands/utility/balance.js
@@ -1,16 +1,40 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { storeTuna } = require('../../helper/TunaStore');
-
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const pool = require('../../helper/db');
+const {Model , DataTypes} = require ('sequelize')
+//Checks balance(Tuna) from Database
 module.exports = {
     data: new SlashCommandBuilder()
       .setName('balance')
-      .setDescription('Set your Guild Wars 2 Account API key.'),
-    async execute(interaction) {
-    let userName = interaction.user.username;
-    let userID =  interaction.user.id;
+      .setDescription('check your balance'),
+    async execute(interaction){
+      let userName = interaction.user.username;
+      let userID = interaction.user.id;
+    const user =  await pool.query('SELECT * FROM tuna  WHERE userID = $1 ',[userID]);
+  
+  if(!user)
+    {
+    
+    let money = 0;
+    let daily = 0;
+    await pool.query('INSERT INTO tuna (userName, userID, money, daily) VALUES ($1, $2, $3, $4)',
+      [userName, userID, money, daily])
+      console.log(`added to tuna table ${userName} ${userID} ${money}`);
+      let balanceEmbed = new EmbedBuilder()
+      .setColor("Random")
+      .setTitle(`${userName}'s Balance`)
+      .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
+      .setDescription(`<:tuna:1320596587373658195>  **${money.rows}** `)
 
-    await storeTuna(userName, userID)
+    interaction.reply({embeds:[balanceEmbed]});
+    }else {
+      let balanceEmbed = new EmbedBuilder()
+      .setColor("Random")
+      .setTitle(`${userName}'s Balance`)
+      .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
+      .setDescription(`<:tuna:1320596587373658195>  **${money.rows}** `)
 
-    interaction.channel.send(`<:tuna:1320596587373658195> ${money}`);
-    },
+    interaction.reply({embeds:[balanceEmbed]});
+
+    }
+  },
 };

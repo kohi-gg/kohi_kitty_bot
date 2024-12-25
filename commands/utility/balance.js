d9f3d0802428c175885d6224664dd31f9f8e7e41
chesidibols <59624109+chesidibols@users.noreply.github.com>
2024-12-25 08:52:38 +0800

    Update balance.js

--- commands/utility/balance.js
+++ commands/utility/balance.js
@@ -3,38 +3,16 @@ const pool = require('../../helper/db');
 const {Model , DataTypes} = require ('sequelize')
 //Checks balance(Tuna) from Database
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName('balance')
-      .setDescription('check your balance'),
-    async execute(interaction){
-      let userName = interaction.user.username;
-      let userID = interaction.user.id;
-    const user =  await pool.query('SELECT * FROM tuna  WHERE userID = $1 ',[userID]);
-  
-  if(!user)
-    {
-    
-    let money = 0;
-    let daily = 0;
-    await pool.query('INSERT INTO tuna (userName, userID, money, daily) VALUES ($1, $2, $3, $4)',
-      [userName, userID, money, daily])
-      console.log(`added to tuna table ${userName} ${userID} ${money}`);
-      let balanceEmbed = new EmbedBuilder()
-      .setColor("Random")
-      .setTitle(`${userName}'s Balance`)
-      .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
-      .setDescription(`<:tuna:1320596587373658195>  **${money.rows}** `)
+  data: new SlashCommandBuilder()
+  .setName('balance')
+  .setDescription('balance'),
+async execute(interaction) {
+    await interaction.deferReply();
+    await interaction.deleteReply();
 
-    interaction.reply({embeds:[balanceEmbed]});
-    }else {
-      let balanceEmbed = new EmbedBuilder()
-      .setColor("Random")
-      .setTitle(`${userName}'s Balance`)
-      .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
-      .setDescription(`<:tuna:1320596587373658195>  **${money.rows}** `)
+    const m = await interaction.channel.send("Ping?");
+m.edit(`Pong isama ang <:tuna:1320596587373658195> ${m.createdTimestamp - interaction.createdTimestamp}ms`);
 
-    interaction.reply({embeds:[balanceEmbed]});
 
-    }
-  },
+},
 };

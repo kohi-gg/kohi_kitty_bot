1ab271c1aceaa8c28ab7bbc690457c97a6c6df8c
chesidibols <59624109+chesidibols@users.noreply.github.com>
2024-12-28 07:39:22 +0800

    Update balance.js
    check balance using mongodb

--- commands/utility/balance.js
+++ commands/utility/balance.js
@@ -1,40 +1,52 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const pool = require('../../helper/db');
-const {Model , DataTypes} = require ('sequelize')
-//Checks balance(Tuna) from Database
+const assert = require("assert");
+//Connect to database
+
+
+// MODELS
+const Data = require("../../helper/data");
+
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName('balance')
-      .setDescription('check your balance'),
-    async execute(interaction){
-      let userName = interaction.user.username;
-      let userID = interaction.user.id;
-    const user =  await pool.query('SELECT * FROM tuna  WHERE userID = $1 ',[userID]);
-  
-  if(!user)
-    {
+  data: new SlashCommandBuilder()
+    .setName('balance')
+    .setDescription('check your balance'),
+  async execute(interaction){
+
     
-    let money = 0;
-    let daily = 0;
-    await pool.query('INSERT INTO tuna (userName, userID, money, daily) VALUES ($1, $2, $3, $4)',
-      [userName, userID, money, daily])
-      console.log(`added to tuna table ${userName} ${userID} ${money}`);
+  let userData = await Data.findOne({userID: interaction.user.id});
+      
+      if(userData){
+        await Data.findOneAndUpdate({userID:interaction.user.id},{name:interaction.user.username}).then(function(){
+            Data.findOne({userID:interaction.user.id}).then(function(result){
+              assert(result.name === interaction.user.username)
+              console.log(`User's username (${interaction.user.username}) is updated to DB`)
+            })
+          }
+        )
+            let balanceEmbed = new EmbedBuilder()
+            .setColor("Random")
+            .setTitle(`${interaction.user.username}'s Balance`)
+            .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
+            .setDescription(`<:tuna:1320596587373658195>  **${userData.tuna}** `)
+            interaction.reply({embeds:[balanceEmbed]});
+            return;
+        };
+
+      if(!userData){
+        let newData = await Data.create({
+          name: interaction.user.username,
+          userID: interaction.user.id,
+          tuna: 0,
+          daily: 0,
+        })
+
+        newData.save().catch(err => console.log(err));
       let balanceEmbed = new EmbedBuilder()
       .setColor("Random")
-      .setTitle(`${userName}'s Balance`)
+      .setTitle(`${interaction.user.username}'s Balance`)
       .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
-      .setDescription(`<:tuna:1320596587373658195>  **${money.rows}** `)
+      .setDescription(`<:tuna:1320596587373658195>  **0** `)
+      interaction.reply({embeds:[balanceEmbed]});
+      }
 
-    interaction.reply({embeds:[balanceEmbed]});
-    }else {
-      let balanceEmbed = new EmbedBuilder()
-      .setColor("Random")
-      .setTitle(`${userName}'s Balance`)
-      .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
-      .setDescription(`<:tuna:1320596587373658195>  **${money.rows}** `)
-
-    interaction.reply({embeds:[balanceEmbed]});
-
-    }
-  },
-};
+  }};

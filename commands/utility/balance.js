6e2ea5210161267e5a906f84808f3884c863877a
chesidibols <59624109+chesidibols@users.noreply.github.com>
2025-01-24 23:24:30 +0800

    Update balance.js
    Remove other user checking of balance

--- commands/utility/balance.js
+++ commands/utility/balance.js
@@ -9,18 +9,9 @@ const Data = require("../../helper/data");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('balance')
-    .setDescription('check your balance')
-    .addUserOption( option =>(
-      option.setName("target")
-      .setDescription('the someone')
-  ))
-  .setDescription('Check someones balance'),
+    .setDescription('check your balance'),
   async execute(interaction){
-
-  const checkUser = interaction.options.getUser("target");
     
-
-      if(!checkUser){
         let userData = await Data.findOne({userID: interaction.user.id});
         if(userData){
           await Data.findOneAndUpdate({userID:interaction.user.id},{name:interaction.user.username}).then(function(){
@@ -37,7 +28,7 @@ module.exports = {
               .setDescription(`<:tuna:1320596587373658195>  **${userData.tuna}** `)
               interaction.reply({embeds:[balanceEmbed]});
               return;
-          };
+          }
   
         if(!userData){
           let newData = await Data.create({
@@ -56,44 +47,6 @@ module.exports = {
           interaction.reply({embeds:[balanceEmbed]});
           return;
           }
-      } else{
-        let otherUser = await Data.findOne({userID: checkUser.id});
-        if(otherUser){
-          await Data.findOneAndUpdate({userID:otherUser},{name:checkUser.username}).then(function(){
-            Data.findOne({userID:checkUser.id}).then(function(result){
-              assert(result.name === checkUser.username)
-              console.log(`User's username (${checkUser.username}) is updated to DB`)
-            })
-          }
-        )
-            let balanceEmbed = new EmbedBuilder()
-            .setColor("#1bb369")
-            .setTitle(`${checkUser.username}'s Balance`)
-            .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
-            .setDescription(`<:tuna:1320596587373658195>  **${otherUser.tuna}** `)
-            interaction.reply({embeds:[balanceEmbed]});
-            return;
-        }
-        
-        };
-
-        if(!otherUser){
-          let newData = await Data.create({
-            name: checkUser.username,
-            userID: otherUser,
-            tuna: 0,
-            daily: 0,
-          })
-  
-          newData.save().catch(err => console.log(err));
-          let balanceEmbed = new EmbedBuilder()
-          .setColor("#1bb369")
-          .setTitle(`${checkUser.username}'s Balance`)
-          .setURL("https://www.youtube.com/watch?v=vNhs9CSI0Vc/")
-          .setDescription(`<:tuna:1320596587373658195>  **0** `)
-          interaction.reply({embeds:[balanceEmbed]});
-          return;
-        }
 
       
   }};

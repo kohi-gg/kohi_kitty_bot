de21fa1fe3f8973a30a9a7d848bc412619d7453e
chesidibols <eddard.joseph@gmail.com>
2025-05-19 08:21:15 +0800

    update require

--- events/dailyAdvice.js
+++ events/dailyAdvice.js
@@ -1,11 +1,11 @@
 const cron = require('node-cron');
 const moment = require('moment-timezone');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const { ChannelType } = require('discord.js');
 
 function startDailyAdvice(client, channelId) {
 	// Schedule: At 8:00 AM Asia/Manila time every day
-	cron.schedule('0 8 * * *', async () => {
+	cron.schedule('30 8 * * *', async () => {
 		const nowPH = moment().tz('Asia/Manila').format('YYYY-MM-DD HH:mm:ss');
 		console.log(`Sending daily advice at ${nowPH} PH time.`);
 
@@ -17,7 +17,7 @@ function startDailyAdvice(client, channelId) {
 			const data = await response.json();
 			const advice = data.slip.advice;
 
-			await channel.send(`📅 **Daily Advice @ 8:00AM PHT:**\n💡 ${advice}`);
+			await channel.send(`📅 **Daily Advice @ 8:30AM PHT:**\n💡 ${advice}`);
 		} catch (error) {
 			console.error('Error sending daily advice:', error);
 		}

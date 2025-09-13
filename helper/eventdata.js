c39039771fa14a103f3a54e1a92518fa16c1cc56
chesidibols <eddard.joseph@gmail.com>
2025-09-13 17:31:56 +0800

    schema for event

---
+++ helper/eventdata.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const eventSchema = new Schema({
+  threadId: { type: String, required: true, unique: true },
+  channelId: { type: String, required: true },
+  messageId: { type: String, required: true },
+  hostId: { type: String, required: true },
+  title: { type: String, required: true },
+  contentType: { type: String, required: true },
+  group: { type: Object, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = model("Event", eventSchema);

e0848db49856bb4ae3a1f7760e5a41280bf5c6d7
chesidibols <eddard.joseph@gmail.com>
2026-03-27 12:57:28 +0800

    restructure

--- commands/utility/create-event.js
+++ commands/utility/create-event.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ThreadAutoArchiveDuration } = require('discord.js');
+const { SlashCommandBuilder, ThreadAutoArchiveDuration } = require('discord.js');
 const Event = require("../../helper/eventdata");
 
 const EVENT_ROLE = {
@@ -14,29 +14,29 @@ const EVENT_ROLE = {
 const GROUP_FIELDS = {
   five: { Tank: 1, BoonDPS: 1, DPS: 3, Fill: 5 },
   ten: { Tank: 2, BoonDPS: 2, DPS: 6, Fill: 10 },
-  tenSubs: {Tank: 10 , BoonDPS: 10, DPS: 30 },
-  unlimited: { Participants: Infinity}
+  tenSubs: { Tank: 10, BoonDPS: 10, DPS: 30 },
+  unlimited: { Participants: Infinity }
 };
 
 const EMOJI_ID = {
-    Tank: "<:heart:1146979167330644019>",
-    BoonDPS: "<:alacrity:1149886586369085510>",
-    DPS: "<:dps:1149886591922352219>",
-    Participants: "👥" // fallback for unlimited
-}
+  Tank: "<:heart:1146979167330644019>",
+  BoonDPS: "<:alacrity:1149886586369085510>",
+  DPS: "<:dps:1149886591922352219>",
+  Participants: "👥"
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('create-event')
-    .setDescription('Create an event (Fractals, Dungeon, Raid, Open World, WvW, Strikes, Convergence)')
+    .setDescription('Create an event')
     .addStringOption(option =>
       option.setName("title")
-        .setDescription("Set your title for this content")
+        .setDescription("Set your title")
         .setRequired(true)
     )
     .addStringOption(option =>
       option.setName("content")
-        .setDescription("Ex. Fractals | Dungeon | Raid | Open World | WvW | Convergence")
+        .setDescription("Select content type")
         .setRequired(true)
         .addChoices(
           { name: "Fractals", value: "fractal" },
@@ -55,20 +55,17 @@ module.exports = {
     const roleID = EVENT_ROLE[contentType];
     const roleMention = `<@&${roleID}>`;
 
-    //checks channel #lfg
     const allowedChannels = [
-    "1371750538626207754",
-    "1364930867591516250"
+      "1371750538626207754",
+      "1364930867591516250"
     ];
 
     if (!allowedChannels.includes(interaction.channel.id)) {
-
-      let MsgEmbed = new EmbedBuilder()
-      .setColor("#c47cca")
-      .setTitle(`Please use <#${'1371750538626207754'}> channel thanks!`);
-
-    await interaction.reply({ embeds: [MsgEmbed], ephemeral: true });
-    return;
+      await interaction.reply({
+        content: `Please use <#1371750538626207754> channel thanks!`,
+        ephemeral: true
+      });
+      return;
     }
 
     // group type
@@ -77,7 +74,7 @@ module.exports = {
       group = GROUP_FIELDS.five;
     } else if (["raid", "strikes"].includes(contentType)) {
       group = GROUP_FIELDS.ten;
-    } else if(["convergence"].includes(contentType)){
+    } else if (["convergence"].includes(contentType)) {
       group = GROUP_FIELDS.tenSubs;
     } else {
       group = GROUP_FIELDS.unlimited;
@@ -87,33 +84,29 @@ module.exports = {
     const signupCounts = {};
     for (const role in group) signupCounts[role] = 0;
 
-    const embed = new EmbedBuilder()
-      .setColor("#c47cca")
-      .setTitle(contentTitle.toUpperCase())
-      .setDescription(`Hosted by: <@${interaction.user.id}> Use \`/sqjoin\` + role to join! \`/leave\` to change role or fully leave`)
-      .addFields(
-        Object.entries(group).map(([role, limit]) => ({
-        name: `${EMOJI_ID[role] || ""} ${role} (${0}/${limit === Infinity ? "∞" : limit})`,
-        value: "-",
-        inline: true,
-        }))
-        );
+    // build plain text message
+    let messageContent = `**${contentTitle.toUpperCase()}**\n`;
+    messageContent += `Hosted by: <@${interaction.user.id}>\n`;
+    messageContent += `Use \`/sqjoin\` + role to join | \`/leave\` to leave/change\n\n`;
 
-    // send event message
+    for (const [role, limit] of Object.entries(group)) {
+      messageContent += `${EMOJI_ID[role] || ""} **${role}** (0/${limit === Infinity ? "∞" : limit})\n`;
+    }
+
+    // send message
     const eventMessage = await interaction.reply({
-      content: roleMention,
-      embeds: [embed],
+      content: `${roleMention}\n${messageContent}`,
       allowedMentions: { roles: [roleID] },
       fetchReply: true
     });
 
-    // create signup thread
+    // create thread
     const thread = await eventMessage.startThread({
       name: `${contentTitle}-signups`,
       autoArchiveDuration: ThreadAutoArchiveDuration.OneDay,
     });
 
-    // save to MongoDB
+    // save to DB
     const newEvent = new Event({
       threadId: thread.id,
       channelId: eventMessage.channel.id,
@@ -128,6 +121,13 @@ module.exports = {
 
     await newEvent.save();
 
-    await thread.send(`This is the signup thread for **${contentTitle}**. Use \`/sqjoin\` + role to join! \`/leave\` to change role or fully leave. For host you can remove any user type \`/remove-user\` + name and reason (required) \`note: only host can close this event, type /close-event to close this event.\``);
+    await thread.send(
+      `Signup thread for **${contentTitle}**\n\n` +
+      `Commands:\n` +
+      `• \`/sqjoin\` + role\n` +
+      `• \`/leave\`\n` +
+      `• Host: \`/remove-user\` + reason\n` +
+      `• Host: \`/close-event\`\n`
+    );
   },
 };
